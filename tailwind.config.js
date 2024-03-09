efdc199ac11c394325002d7e7e58f89639df35ea
Hannan Fikri <hannanfikri510@gmail.com>
2024-03-09 13:23:52 +0800

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,29 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+// init setup config
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     `./src/pages/**/*.{js,jsx,ts,tsx}`,
+//     `./src/components/**/*.{js,jsx,ts,tsx}`,
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         'sans': ['Syne-Regular', 'sans-serif'],
+//         'syne-bold': ['Syne-Bold', 'sans-serif'],
+//       }
+//     },
+//   },
+//   variants: {
+//     extend: {
+//       fontFamily: ['responsive'],
+//     },
+//   },
+//   plugins: [],
+// }
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
   content: [
     `./src/pages/**/*.{js,jsx,ts,tsx}`,
     `./src/components/**/*.{js,jsx,ts,tsx}`,
@@ -18,4 +42,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+}); 
