31edfd651e86caf03a3e65e821396fbb3f206304
Hannan Fikri <hannanfikri510@gmail.com>
2024-03-09 15:37:40 +0800

    resolve import

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import "./src/styles/global.css"
 import React from 'react';
-import Layout from './src/components/Layout';
+import Layout from "./src/components/layout/Layout.tsx"
 
 export const wrapRootElement = ({ element }) => {
   return <Layout>{element}</Layout>;
