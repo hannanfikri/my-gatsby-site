8686535f5d09cd22830b9ba7c47cddef46adcb16
Hannan Fikri <hannanfikri510@gmail.com>
2024-03-10 01:49:34 +0800

    enhance layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,8 +1,10 @@
 import { ThemeProvider } from "@material-tailwind/react";
 import React from "react";
+import { theme } from "../../themes/theme";
 
 const Layout: React.FC = ({ children }) => {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  const customTheme = { theme };
+  return <ThemeProvider value={customTheme}>{children}</ThemeProvider>;
 };
 
 export default Layout;
