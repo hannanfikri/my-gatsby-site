3ce627c554acdf6107e3fc049006ba4d0dde4501
Hannan Fikri <hannanfikri510@gmail.com>
2024-03-09 13:23:34 +0800

    update layout

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,6 @@
+import { ThemeProvider } from "@material-tailwind/react";
+import React from "react";
+
+const Layout: React.FC = ({ children }) => {
+  return <ThemeProvider>{children}</ThemeProvider>;
+};
